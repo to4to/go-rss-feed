7b63653c4f12385fa505c34844f08105199f2696
to4to <justjustifying@outlook.com>
2024-05-27 16:57:54 +0530

    chore: Update go.mod with google/uuid v1.6.0 dependency

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,11 +1,12 @@
 -- +goose Up
 
 CREATE TABLE users(
-    id UUID PRIMARY KEY
+    id UUID PRIMARY KEY,
+    
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    -- deleted_at TIMESTAMP
     name TEXT NOT NULL
-    created_at TIMESTAMP NOT NULL
-    updated_at TIMESTAMP NOT NULL
-    deleted_at TIMESTAMP
 );
 
 
