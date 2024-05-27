4a6787ebc519d46af14b73238b6e8483f6f0eb9f
to4to <justjustifying@outlook.com>
2024-05-27 15:23:42 +0530

    feat: Add users table to SQL schema
    The commit adds a new table called "users" to the SQL schema. The table includes columns for id, name, created_at, updated_at, and deleted_at.

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+
+CREATE TABLE users(
+    id UUID PRIMARY KEY
+    name TEXT NOT NULL
+    created_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL
+    deleted_at TIMESTAMP
+);
+
+
+
+
+
+-- +goose Down
+
+DROP TABLE users;
