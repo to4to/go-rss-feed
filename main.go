ad4e5bbb5b88046a58b33febea5bb873b9feb8b0
to4to <justjustifying@outlook.com>
2024-05-27 14:09:11 +0530

    Update go-chi and godotenv dependencies to latest versions

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,4 +21,30 @@ func main() {
 		log.Fatal("PORT Not Found")
 	}
 	fmt.Println("Running At Port: ", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(
+		cors.Handler(cors.Options{
+
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300,
+		}),
+	)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	fmt.Printf("Server Starting At Port: %v ", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
