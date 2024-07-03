d03578076f20e25d9a28f8f7378e9f53e2b25376
to4to <justjustifying@outlook.com>
2024-07-03 10:15:27 +0530

    chore: Add CreateUser handler to handler_user.go file

--- handler/handler_user.go
+++ handler/handler_user.go
@@ -1,17 +1,47 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/to4to/go-rss-feed/helper"
+	"github.com/to4to/go-rss-feed/internal/db"
 	"github.com/to4to/go-rss-feed/models"
 )
 
-
-type LocalApiConfig struct{
+type LocalApiConfig struct {
 	models.ApiConfig
 }
-func (l *LocalApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
 
-	helper.RespondWithJSON(w ,200,struct{}{})
+func (l *LocalApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type paramaters struct {
+		Name string `json:"name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := paramaters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		helper.RespondWithError(w, 400, fmt.Sprintf("Error Parsing JSON", err))
+		return
+	}
+
+	user,err:= l.DB.CreateUser(r.Context(),db.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now(),
+		Name: params.Name,
+	})
+
+
+	if err!=nil{
+		helper.RespondWithError(w, 400, fmt.Sprintf("Couldn't Create User", err))
+		return
+	}
+	helper.RespondWithJSON(w, 200, user)
 }
